15ed8bb374c3aed9b04e0b5fbb1409d4b08315dd
RuslanPr0g <59767834+RuslanPr0g@users.noreply.github.com>
2020-07-20 18:10:35 +0300

    setup sass

--- nd/src/App.js
+++ nd/src/App.js
@@ -5,10 +5,19 @@ import logo from "./img/ND-logo.png";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="ND" className="logo-img" />
-        <h1>New Dimensions</h1>
+      <header>
+        <div className="header-logo">
+          <img src={logo} alt="ND" className="logo-img" />
+          <h1>New Dimensions</h1>
+        </div>
+        <div className="header-right">
+          <a href="#">How it works</a>
+          <a href="#">Prices</a>
+          <a href="#">Support</a>
+        </div>
       </header>
+      <main>Content</main>
+      <footer>Footer</footer>
     </div>
   );
 }
